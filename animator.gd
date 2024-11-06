697c6a85d98f82f8364c1c9e75e4ce432fd3082f
Hugo <148060520+gr8alpaca@users.noreply.github.com>
2024-11-05 23:53:07 -0500

    Update animator.gd

--- animator.gd
+++ animator.gd
@@ -1,17 +1,20 @@
 @tool
 class_name Animator extends Node
 const GROUP: StringName = &"animator"
+
 const SIGNAL_HIDE: StringName = GROUP + &"_hide"
 const SIGNAL_SHOW: StringName = GROUP + &"_show"
 const SIGNAL_FINISHED: StringName = GROUP + &"_finished"
 
 
-@export_custom(0,"", PROPERTY_USAGE_EDITOR)
+const DEBUG_COLOR: Color = Color(1, 0.4118, 0.7059, 0.5)
+
+@export_custom(0, "", PROPERTY_USAGE_EDITOR)
 var visible: bool = true:
 	set(val):
 		visible = val
 		if visible and elapsed == 0.0: return
-		set_process(true)
+		set_process(parent != null)
 		
 
 @export_category("Tweening")
@@ -19,159 +22,190 @@ var visible: bool = true:
 	set(val):
 		side = val
 		axis = side % 2
+		update_pivot()
 		calculate_delta()
 
 
-@export_range(0.1, 5.0, 0.1, "or_greater", "suffix:sec", ) 
+@export_range(0.1, 5.0, 0.1, "or_greater", "suffix:sec", )
 var duration: float = 1.0:
-	set(val): duration = val; set_elapsed(elapsed)
+	set(val): duration = val; set_elapsed(elapsed);
 
-@export_range(0.1, 3.0, 0.05, "or_greater",  ) 
+@export_range(0.1, 3.0, 0.05, "or_greater", )
 var hide_time_scale: float = 1.5
 
-@export_group("Position")
 @export var trans_type: Tween.TransitionType = Tween.TRANS_BACK
 @export var ease_type: Tween.EaseType = Tween.EASE_IN_OUT
 
-
 @export_group("Scale")
 
-@export var min_scale: float = 1.0
+@export_range(0.05, 1.0, 0.05,) var min_scale: float = 0.3:
+	set(val): min_scale = clampf(val, 0.01, max_scale); set_elapsed(elapsed);
+@export var max_scale: float = 1.0:
+	set(val): max_scale = maxf(val, min_scale)
+	
+@export var scale_both_axis: bool = true
+@export var auto_adjust_pivot: bool = true
+
 @export var trans_type_scale: Tween.TransitionType = Tween.TRANS_BACK
 @export var ease_type_scale: Tween.EaseType = Tween.EASE_IN_OUT
 
 var parent: Control: set = set_parent
 var rect: ReferenceRect = ReferenceRect.new()
 
-var screen:= Vector2(ProjectSettings["display/window/size/viewport_width"], ProjectSettings["display/window/size/viewport_height"])
-
-var axis : int = Vector2.AXIS_X
+var screen: Vector2 = Vector2(ProjectSettings["display/window/size/viewport_width"], ProjectSettings["display/window/size/viewport_height"])
+var axis: int = Vector2.AXIS_X
 
 @export_category("Debug")
 @export var debug_rect_visible: bool = true:
 	set(val): debug_rect_visible = val; rect.visible = val;
-	
+
 var offset: float = 0.0
-@export var max_scale: float = 1.0
+
 
 @export_custom(0, "", PROPERTY_USAGE_EDITOR)
 var elapsed: float = 0.0: set = set_elapsed
-
-#var rid: RID
+var delay_sec: float = 0.0
 
 func _init() -> void:
 	set_process(false)
+	set_physics_process(false)
 	rect.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	rect.border_color = Color(Color.FUCHSIA, 0.8)
-	rect.border_width = 1.0
-	add_child(rect, false, Node.INTERNAL_MODE_FRONT)
+	rect.border_color = Color(1, 0.4118, 0.7059, 0.5)
+	rect.border_width = 2.0
+	add_child(rect, false, Node.INTERNAL_MODE_DISABLED)
 	
-func show() -> void:
+func show(delay_sec: float = 0.0) -> void:
+	self.delay_sec = delay_sec
 	visible = true
+
 func hide() -> void:
 	visible = false
 
 
 func _process(delta: float) -> void:
+	if delay_sec > 0.0:
+		delay_sec -= maxf(0.0, delta)
+		return
 	elapsed = elapsed - delta if visible else elapsed + (delta * hide_time_scale)
 	if (visible and elapsed == 0.0) or (not visible and elapsed == duration):
 		set_process(false)
 
-#func _physics_process(delta: float) -> void:
-	#align_rect()
-	
+
+func _physics_process(delta: float) -> void:
+	align_rect()
+
+
 func calculate_delta() -> void:
+	if not parent: return
 	offset = -rect.global_position[axis] - (rect.size[axis] * min_scale) if side < 2 else screen[axis] - rect.global_position[axis]
 	set_elapsed(elapsed)
 
 
 func align_rect() -> void:
 	if not parent: return
-	var delta_changed: bool = false
-	
-	
+	var is_changed: bool
 	if rect.size != parent.size:
 		rect.size = parent.size
-		delta_changed = true
-	
+		update_pivot()
+		is_changed = true
 	
 	if elapsed == 0.0 and rect.global_position != parent.global_position:
 		rect.global_position = parent.global_position
-		delta_changed = true
-		
-	if delta_changed: 
+		is_changed = true
+	
+	if is_changed:
 		calculate_delta()
+
+
+func update_pivot() -> void:
+	if not auto_adjust_pivot or not parent: return
+	parent.pivot_offset.x = parent.size.x * 0.5 if side % 2 == 1 else parent.size.x * float(side == SIDE_RIGHT)
+	parent.pivot_offset.y = parent.size.y * 0.5 if side % 2 == 0 else parent.size.y * float(side == SIDE_BOTTOM)
 
 
 func set_elapsed(val: float) -> void:
 	elapsed = clampf(val, 0.0, duration)
-	if not parent: return
+	if not parent or not parent.is_node_ready(): return
 	parent.visible = elapsed != duration or Engine.is_editor_hint()
-	parent.global_position[axis^1] = rect.global_position[axis^1]
+	parent.global_position[axis ^ 1] = rect.global_position[axis ^ 1]
 	parent.global_position[axis] = Tween.interpolate_value(rect.global_position[axis], offset, elapsed, duration, trans_type, ease_type)
+
 	if min_scale < max_scale:
-		parent.scale[axis^1] = max_scale
 		parent.scale[axis] = Tween.interpolate_value(max_scale, min_scale - max_scale, elapsed, duration, trans_type_scale, ease_type_scale)
+		parent.scale[axis ^ 1] = parent.scale[axis] if scale_both_axis else max_scale
+		parent.global_position[axis ^ 1] = rect.global_position[axis ^ 1] + (max_scale - parent.scale[axis ^ 1]) * rect.size[axis ^ 1] / 2.0
+
 
 func set_parent(val: Control) -> void:
-	if is_instance_valid(parent):
-		parent.disconnect(&"resized", _on_parent_resize)
-		parent.remove_meta(GROUP)
-		for signal_name : StringName in [SIGNAL_HIDE, SIGNAL_SHOW, SIGNAL_FINISHED]:
+	if parent:
+		#parent.remove_meta(GROUP)
+		for signal_name: StringName in [SIGNAL_HIDE, SIGNAL_SHOW, SIGNAL_FINISHED]:
 			parent.remove_user_signal(signal_name)
 	
 	parent = val
 	
 	if parent:
-		parent.set_meta(GROUP, self as Animator)
-		parent.connect(&"resized", _on_parent_resize)
-		for signal_name : StringName in [SIGNAL_HIDE, SIGNAL_SHOW, SIGNAL_FINISHED]:
-			parent.add_user_signal(signal_name)
+		for signal_name: StringName in [SIGNAL_HIDE, SIGNAL_SHOW, SIGNAL_FINISHED]:
+			parent.add_user_signal(signal_name, [ {name=&"delay_sec", type=TYPE_FLOAT}] if signal_name == SIGNAL_SHOW else [])
 		parent.connect(SIGNAL_HIDE, hide)
 		parent.connect(SIGNAL_SHOW, show)
-			#var callable:= Callable(self, signal_name.trim_prefix(GROUP + "_"))
-			#if callable.is_valid() and not parent.is_connected(signal_name, callable):
-				#parent.connect(signal_name, callable)
-			
-		align_rect()
-		calculate_delta()
 		
+		align_rect()
+	
 	rect.visible = parent != null and debug_rect_visible
+
+
+func set_trans(trans_mode: Tween.TransitionType) -> Animator:
+	self.trans_type = trans_mode
+	return self
+func set_ease(ease_mode: Tween.EaseType) -> Animator:
+	self.ease_type = ease_mode
+	return self
+func set_duration(time_sec: float) -> Animator:
+	self.duration = time_sec
+	return self
+func set_side(side: Side) -> Animator:
+	self.side = side
+	return self
+func set_scale(min_scale: float = 0.3, max_scale: float = 1.0, scale_both_axis: bool = true) -> Animator:
+	self.min_scale = min_scale
+	self.max_scale = max_scale
+	self.scale_both_axis = scale_both_axis
+	return self
 
 
 func _notification(what: int) -> void:
 	match what:
-		NOTIFICATION_PARENTED:
-			parent = get_parent() as Control
-		NOTIFICATION_UNPARENTED:
-			parent = null
-			elapsed = 0.0
-		NOTIFICATION_PHYSICS_PROCESS:
-			align_rect()
-		NOTIFICATION_EDITOR_PRE_SAVE:
-			print("Pre save called!")
+		NOTIFICATION_PARENTED when get_parent().is_node_ready():
+			set_parent.call_deferred(get_parent())
+		NOTIFICATION_PARENTED when not get_parent().is_node_ready():
+			get_parent().ready.connect(set_parent.bind(get_parent() as Control), CONNECT_DEFERRED | CONNECT_ONE_SHOT)
+		NOTIFICATION_EXIT_TREE:
+			if parent: parent.set_meta(GROUP, null)
+			
 		NOTIFICATION_READY:
-			#rid = RenderingServer.canvas_item_create()
-			#RenderingServer.canvas_item_set_parent(rid, get_viewport().world_2d.canvas)
 			if not Engine.is_editor_hint():
 				get_window().size_changed.connect(_on_window_size_changed)
 				_on_window_size_changed()
-				
-				#create_tween().set_loops(0).tween_callback(align_rect).set_delay(0.1)
-		
+			
+			else:
+				var selection:= EditorInterface.get_selection()
+				var inspector:= EditorInterface.get_inspector()
+				#inspector.
+				inspector.edited_object_changed.connect(_on_edited_object_changed.bind(inspector, selection))
 
+
+func _on_edited_object_changed(inpsector: EditorInspector, selection: EditorSelection) -> void:
+	if not parent or inpsector.get_edited_object() != self: return
+	selection = EditorInterface.get_selection()
+	selection.add_node(parent)
+	selection.remove_node(self)
+	print("Transformable Nodes: %s" % selection.get_transformable_selected_nodes())
 
 func _on_parent_resize() -> void:
 	calculate_delta()
 
-#func redraw()->void:
-	#RenderingServer.canvas_item_clear(rid)
-	#if not parent: return
-	#var r:= parent.get_global_rect()
-	#RenderingServer.canvas_item_add_polyline(rid, [r.position, Vector2(r.end.x, r.position.y), r.end, Vector2(r.position.x, r.end.y), r.position], [Color.DIM_GRAY],)
-	#RenderingServer.canvas_item_add_polyline(rid, [r.position, Vector2(r.end.x, r.position.y), r.end, Vector2(r.position.x, r.end.y), r.position], [Color.DIM_GRAY],)
 
 func _on_window_size_changed() -> void:
 	screen = rect.get_viewport_rect().size
 	calculate_delta()
-	
