c3c47bcced6e94dc11c8f617bb09997c98b05d51
Hugo <148060520+gr8alpaca@users.noreply.github.com>
2024-11-07 15:10:49 -0500

    changing target to an export

--- animator.gd
+++ animator.gd
@@ -1,12 +1,14 @@
 @tool
 class_name Animator extends Node
-const GROUP: StringName = &"animator"
+const GROUP: StringName = &"_animator"
 
 const SIGNAL_HIDE: StringName = GROUP + &"_hide"
 const SIGNAL_SHOW: StringName = GROUP + &"_show"
 const SIGNAL_FINISHED: StringName = GROUP + &"_finished"
 
 const DEBUG_COLOR: Color = Color(1, 0.4118, 0.7059, 0.5)
+
+
 
 var visible: bool = true:
 	set(val):
@@ -15,37 +17,33 @@ var visible: bool = true:
 		set_process(parent != null)
 
 
-@export_category("Tweening")
+@export var target: Control : set = set_target
 
-@export_custom(PROPERTY_HINT_ENUM, "Left Side:0,Top Side:1,Right Side:2,Bottom Side:3,Custom Rect:4") 
-var side: Side = SIDE_LEFT:
+#const CUSTOM_TARGET: int = 4
+@export var side: Side = SIDE_LEFT:
 	set(val):
 		side = val
-		if side == 4:
-			visible_rect.queue_redraw.call_deferred()
-			update_pivot()
-			return
-		
-		
 		axis = side % 2
 		update_pivot()
 		calculate_delta()
+		
+@export var hide_on_finish: bool = true
 
-
+@export_category("Tweening")
 @export_range(0.1, 5.0, 0.1, "or_greater", "suffix:sec", )
 var duration: float = 1.0:
 	set(val): duration = val; set_elapsed(elapsed);
 
-@export_range(0.1, 3.0, 0.05, "or_greater", )
-var hide_time_scale: float = 1.5
+@export_range(0.1, 3.0, 0.1, "or_greater", "suffix:x")
+var hide_time_scale: float = 2.0
 
-@export var trans_type: Tween.TransitionType = Tween.TRANS_BACK
-@export var ease_type: Tween.EaseType = Tween.EASE_IN_OUT
+@export var trans_type_show: Tween.TransitionType = Tween.TRANS_ELASTIC
+@export var ease_type_show: Tween.EaseType = Tween.EASE_IN
 
-@export var hide_on_finish: bool = true
+@export var trans_type_hide: Tween.TransitionType = Tween.TRANS_BOUNCE
+@export var ease_type_hide: Tween.EaseType = Tween.EASE_OUT
 
 @export_group("Scale")
-
 @export_range(0.05, 1.0, 0.05,) var min_scale: float = 0.3:
 	set(val): min_scale = clampf(val, 0.01, max_scale); set_elapsed(elapsed);
 	
@@ -61,7 +59,7 @@ var hide_time_scale: float = 1.5
 var parent: Control: set = set_parent
 var visible_rect: ReferenceRect = ReferenceRect.new()
 
-var target: ReferenceRect: set = set_target
+
 
 var screen: Vector2 = Vector2(ProjectSettings["display/window/size/viewport_width"], ProjectSettings["display/window/size/viewport_height"])
 var axis: int = Vector2.AXIS_X
@@ -72,8 +70,6 @@ var axis: int = Vector2.AXIS_X
 
 var offset: float = 0.0
 
-
-@export_custom(0, "", PROPERTY_USAGE_EDITOR) 
 var elapsed: float = 0.0: set = set_elapsed
 var delay_sec: float = 0.0
 
@@ -86,16 +82,10 @@ func _init() -> void:
 	visible_rect.border_width = 2.0
 	add_child(visible_rect, false, Node.INTERNAL_MODE_DISABLED)
 	
-	
-	target.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	target.border_color = Color(1, 0.4118, 0.7059, 0.5)
-	target.border_width = 2.0
-	target.name = &"TargetRect"
-	#target.call_deferred.connect(&"tree_entered")
-	add_child(target, true, Node.INTERNAL_MODE_DISABLED)
-	
 
-
+func set_target(val: Control) -> void:
+	target = val
+	notify_property_list_changed()
 
 
 func show(delay_sec: float = 0.0) -> void:
@@ -119,10 +109,6 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
-	if side == 4:
-		pass
-		return
-		
 	align_rect()
 
 
@@ -135,7 +121,7 @@ func calculate_delta() -> void:
 func align_rect() -> void:
 	if not parent: return
 	var is_changed: bool
-	if visible_rect.size != parent.size:
+	if visible_rect.size != parent.size and elapsed == 0.0:
 		visible_rect.size = parent.size
 		update_pivot()
 		is_changed = true
@@ -160,16 +146,17 @@ func set_elapsed(val: float) -> void:
 	if not parent or not parent.is_node_ready(): return
 	parent.visible = elapsed != duration or Engine.is_editor_hint() or not hide_on_finish
 	
-	if side == 4:
-		parent.global_position = Tween.interpolate_value(visible_rect.global_position, target.global_position - visible_rect.global_position, elapsed, duration, trans_type, ease_type)
-		parent.scale = Tween.interpolate_value(visible_rect.scale, visible_rect.scale - target.scale, elapsed, duration, trans_type, ease_type)
-		parent.rotation = Tween.interpolate_value(visible_rect.rotation, visible_rect.rotation - target.rotation, elapsed, duration, trans_type, ease_type)
-		parent.size = Tween.interpolate_value(visible_rect.size, visible_rect.size - target.size, elapsed, duration, trans_type, ease_type)
+	var tween_trans: Tween.TransitionType = trans_type_show if visible else trans_type_hide
+	var tween_ease: Tween.EaseType = ease_type_show if visible else ease_type_hide
+	
+	if target:
+		for prop: StringName in [&"global_position", &"scale", &"rotation", &"size"]:
+			parent[prop] = Tween.interpolate_value(visible_rect[prop], target[prop] - visible_rect[prop], elapsed, duration, tween_trans, tween_ease)
 		return
 		
 	
 	parent.global_position[axis ^ 1] = visible_rect.global_position[axis ^ 1]
-	parent.global_position[axis] = Tween.interpolate_value(visible_rect.global_position[axis], offset, elapsed, duration, trans_type, ease_type)
+	parent.global_position[axis] = Tween.interpolate_value(visible_rect.global_position[axis], offset, elapsed, duration, tween_trans, tween_ease)
 
 	if min_scale < max_scale:
 		parent.scale[axis] = Tween.interpolate_value(max_scale, min_scale - max_scale, elapsed, duration, trans_type_scale, ease_type_scale)
@@ -194,14 +181,12 @@ func set_parent(val: Control) -> void:
 	
 	visible_rect.visible = parent != null and debug_rect_visible
 
-func set_target(val: ReferenceRect) -> void:
-	if not val: return
-	target = val
 	
 func _notification(what: int) -> void:
 	match what:
 		NOTIFICATION_PARENTED when get_parent().is_node_ready():
 			set_parent.call_deferred(get_parent())
+			
 		NOTIFICATION_PARENTED when not get_parent().is_node_ready():
 			get_parent().ready.connect(set_parent.bind(get_parent() as Control), CONNECT_DEFERRED | CONNECT_ONE_SHOT)
 			
@@ -209,12 +194,16 @@ func _notification(what: int) -> void:
 			parent = null
 			
 		NOTIFICATION_READY:
-			if not target: 
-				target = ReferenceRect.new()
 			if not Engine.is_editor_hint():
 				get_window().size_changed.connect(_on_window_size_changed)
 				_on_window_size_changed()
-
+				visible = false
+				
+		NOTIFICATION_EDITOR_PRE_SAVE:
+			parent.remove_meta(GROUP)
+		NOTIFICATION_EDITOR_POST_SAVE:
+			parent.set_meta(GROUP, self)
+		
 
 func _on_window_size_changed() -> void:
 	screen = visible_rect.get_viewport_rect().size
@@ -222,17 +211,28 @@ func _on_window_size_changed() -> void:
 
 func _validate_property(property: Dictionary) -> void:
 	match property.name:
-		&"visible":
+		&"visible", &"delay_sec":
 			property.usage |= PROPERTY_USAGE_EDITOR
-	
-	
+			
+		&"elapsed":
+			property.usage |= PROPERTY_USAGE_EDITOR
+			property.hint = PROPERTY_HINT_RANGE
+			property.hint_string = "0.0,%01.01f,0.1,suffix:sec," % duration
+			
+		&"Scale" when property.usage&PROPERTY_USAGE_GROUP and target:
+			property.name = &""
+			
+		&"side", &"max_scale", &"min_scale", &"scale_both_axis", &"trans_type_scale", &"ease_type_scale" when target:
+			property.usage &= ~(PROPERTY_USAGE_EDITOR)
+
+
 #region Setter Helpers
 
 func set_trans(trans_mode: Tween.TransitionType) -> Animator:
-	self.trans_type = trans_mode
+	self.trans_type_show = trans_mode
 	return self
 func set_ease(ease_mode: Tween.EaseType) -> Animator:
-	self.ease_type = ease_mode
+	self.ease_type_show = ease_mode
 	return self
 func set_duration(time_sec: float) -> Animator:
 	self.duration = time_sec
